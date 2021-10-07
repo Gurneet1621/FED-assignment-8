2b8c9da4066183cd167ba48413f89bb06f8a98fa
Gurneet1621 <80595343+Gurneet1621@users.noreply.github.com>
2021-10-07 19:36:11 +0530

    Add files via upload

---
+++ web-assingment-master/Methods/sendEmail.js
@@ -0,0 +1,19 @@
+const sgMail=require("@sendgrid/mail");
+
+const API_KEY=process.env.KEY;
+sgMail.setApiKey(API_KEY);
+
+function sendMail(to,subject,text)
+{
+
+    const message= {
+    
+    to,
+        from:"gurneet1621.cse19@chitkara.edu.in",
+        subject,
+        text,
+    }
+
+    sgMail.send(message).then((res)=>console.log("sent")).catch((e)=>console.log(e.message));
+}
+module.exports ={sendMail};
